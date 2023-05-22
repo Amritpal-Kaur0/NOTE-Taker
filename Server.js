acece13eae3aae843a0fda8c84f1a80f9a352850
Amrit <128442182+Amritpal-Kaur0@users.noreply.github.com>
2023-05-22 15:39:41 +0530

    process.env updated

--- Server.js
+++ Server.js
@@ -19,6 +19,6 @@ const api_routes =require('./Routing/html-route.js')
 app.use(api_routes)
 
 //Listening Port
-app.listen(PORT,()=>{
+app.listen(process.env.PORT,3000,()=>{
     console.log(`You are listening the server At http://localhost:${PORT}`)
 })

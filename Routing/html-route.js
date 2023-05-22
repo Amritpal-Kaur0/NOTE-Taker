835ddc203238bc92e2865e9b18b1938bfa63b1f8
Amrit <128442182+Amritpal-Kaur0@users.noreply.github.com>
2023-05-22 19:52:15 +0530

    comments updated

--- Routing/html-route.js
+++ Routing/html-route.js
@@ -1,9 +1,10 @@
 const Router =require('express').Router(); 
 const path =require('path');
-
+//home page html
     Router.get('/',(req,res)=>{
         res.sendFile(path.join(__dirname, './public/index.html'));
     })
+//notes page html
     Router.get('/notes',(req,res)=>{
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     })
